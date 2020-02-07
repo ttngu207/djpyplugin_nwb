a10287351382942e5da50a0e8fbfa16c81766e45
Thinh Nguyen <37779736+ttngu207@users.noreply.github.com>
2020-02-07 11:52:12 -0600

    design round 1

---
+++ dj_nwb_adapter/__init__.py
@@ -0,0 +1,4 @@
+from .nwb_adapter import (nwb_file, device, patch_clamp_series, ic_electrode)
+
+pkg_name = 'dj_nwb_adapter'
+__version__ = '0.0.1'
---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+import pathlib
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open(pathlib.Path(__file__).parent / 'dj_nwb_adapter' / '__init__.py') as f:
+    exec(f.read())
+
+setuptools.setup(
+    name=pkg_name,
+    version=__version__,
+    author="Thinh Nguyen",
+    author_email="thinh@vathes.com",
+    description="Unofficial DataJoint NWB type plugin.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ttngu207/djpyplugin_nwb",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    setup_requires=['setuptools_certificate'],
+    install_requires=['datajoint', 'pynwb'],
+    entry_points={
+        'datajoint.plugins': 'attribute_adapter = {}'.format(pkg_name)
+    },
+)
