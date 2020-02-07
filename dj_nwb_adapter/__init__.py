06b429192866301ceb3fc62c45d92c4004588ede
Thinh Nguyen <37779736+ttngu207@users.noreply.github.com>
2020-02-07 11:58:24 -0600

    Update __init__.py

--- dj_nwb_adapter/__init__.py
+++ dj_nwb_adapter/__init__.py
@@ -1,4 +1,4 @@
-from .nwb_adapter import (nwb_file, device, patch_clamp_series, ic_electrode)
+from dj_nwb_adapter.nwb_adapter import (nwb_file, device, patch_clamp_series, ic_electrode)
 
 pkg_name = 'dj_nwb_adapter'
 __version__ = '0.0.1'
